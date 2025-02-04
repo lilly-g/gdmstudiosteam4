9e82ed9232167fb20ff71acb0c4419c09590c9c3
kana797 <wangzirun68@gmail.com>
2025-02-03 19:19:32 -0500

    improved reloading time after death

--- Assets/Code/Scripts/Spike.cs
+++ Assets/Code/Scripts/Spike.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Spike : MonoBehaviour
 {
-    // This method is called when another collider enters the trigger collider attached to this GameObject
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Check if the object colliding with the spike has the "Player" tag
         if (other.CompareTag("Player"))
         {
-            // The player dies immediately. Reload the current scene.
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            Debug.Log("player is kill");
+            //make the player vanish, can instead be a vanishing animation in the future
+            //so that it looks smoother
+            other.gameObject.SetActive(false);
+            //reload the scene
+            StartCoroutine(ReloadScene());
         }
     }
+
+    private IEnumerator ReloadScene()
+    {
+        yield return null; // Wait for the next frame before reloading
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
+
 
