8b2018d9b9a60ca5a4a479d1f2518792250e6d56
kana797 <wangzirun68@gmail.com>
2025-03-27 15:17:23 -0400

    Added Dashing particle effect(To be imprvoved). The change can be applied by Apply changes in the Player prefab in scene DashEffectTest

---
+++ Assets/DashEffect.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class DashEffect : MonoBehaviour
+{
+    public ParticleSystem dashEffect;
+
+    private PlayerController playerController;
+    private bool wasDashingLastFrame = false;
+    private bool isDashing;
+
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        dashEffect.Stop();
+    }
+
+    void Update()
+    {
+        if (playerController == null) return;
+
+        isDashing = playerController.isDashing;
+
+        if (isDashing)
+        {
+            if (!wasDashingLastFrame && dashEffect != null && !dashEffect.isPlaying)
+            {
+                dashEffect.Play();
+            }
+        }
+        else
+        {
+            if (wasDashingLastFrame && dashEffect != null && dashEffect.isPlaying)
+            {
+                dashEffect.Stop();
+            }
+        }
+
+        wasDashingLastFrame = isDashing;
+    }
+}
